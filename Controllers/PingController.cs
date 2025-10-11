4e8dba27da15428c9b4cc6fd51e6e4bce72ccba7
rafael1209 <mr.chasmanrafael@gmail.com>
2025-10-12 00:28:43 +0300

    Add project files.

---
+++ Controllers/PingController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MongoBackupBackend.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class PingController(ILogger<PingController> logger) : ControllerBase
+    {
+        /// <summary>
+        /// Проверка работы сервера.
+        /// </summary>
+        /// <returns>Простой ответ "pong"</returns>
+        /// <response code="200">Сервер работает и возвращает pong</response>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult PingServer()
+        {
+            logger.LogInformation("Ping received at {Time}", DateTime.UtcNow);
+            return Ok("pong");
+        }
+    }
+}
